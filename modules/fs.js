f17ac883dabf14dbba5db9918286050515fed46e
Flavio Macedo <fhenrique609@gmail.com>
2025-01-23 12:36:03 -0300

    Criando um arquivo com fs

--- modules/fs.js
+++ modules/fs.js
@@ -9,3 +9,16 @@ fs.mkdir(path.join(__dirname, "/test"), (error) => {
 
   console.log("Pasta criada com sucesso!");
 });
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro: ", error);
+    }
+
+    console.log("Arquivo criado com sucesso!");
+  }
+);
