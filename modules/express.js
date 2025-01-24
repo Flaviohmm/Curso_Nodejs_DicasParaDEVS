ab2b5cc0e00ad30903aa564136940432e8cc6c97
Flavio Macedo <fhenrique609@gmail.com>
2025-01-24 15:16:18 -0300

    Update and Delete API Rest

--- modules/express.js
+++ modules/express.js
@@ -5,15 +5,23 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-  res.contentType("application/html");
-  res.status(200).send("<h1>Hello World</h1>");
-});
-
 app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find({});
+
     res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).send(error.message);
   }
@@ -22,7 +30,32 @@ app.get("/users", async (req, res) => {
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
+
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndDelete(id);
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
